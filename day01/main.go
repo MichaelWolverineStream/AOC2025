d0ceea82d99638af28296c2228fd9f7dd5f9dfd0
Michael Järvå <michael.jarva@gmail.com>
2025-12-01 15:06:32 +1100

    init

---
+++ day01/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func solvePart1(inputFile string) int {
+	data := loadDayData(inputFile)
+	if data[0] == "row1" {
+		return 1
+	}
+	return 0
+}
+
+// func solvePart2(inputFile string) int {
+// 	return 0
+// }
+
+func loadDayData(inputFile string) []string {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var data []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		data = append(data, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
+func main() {
+	fmt.Println(solvePart1("sample_input.txt"))
+	// fmt.Println(solvePart1("input1.txt"))
+	// fmt.Println(solvePart2("input1.txt"))
+}
